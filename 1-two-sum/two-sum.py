74e926a438bdd35329f8ca1c82720971b549eb08
Eyader Tsehayu <eyadertsehayu@gmail.com>
2025-11-01 12:52:41 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (10.40%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,14 +1,24 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        twoSumIndices=[]
-        for num in nums:
-            diff = target - num
+
+        hashmap = {}
+        for i,v in enumerate(nums):
+            diff = target - v
+
+            if diff in hashmap:
+                return [i,hashmap[diff]]
+            hashmap[v] = i
+
+
+        # twoSumIndices=[]
+        # for num in nums:
+        #     diff = target - num
             
-            if diff in nums and diff != num:
-                twoSumIndices.append(nums.index(num))
-                twoSumIndices.append(nums.index(diff))
-                return twoSumIndices
-            elif diff == num and nums.count(diff) > 1:    
-                twoSumIndices = [i for i,n in enumerate(nums) if n == diff]
-                return twoSumIndices
+        #     if diff in nums and diff != num:
+        #         twoSumIndices.append(nums.index(num))
+        #         twoSumIndices.append(nums.index(diff))
+        #         return twoSumIndices
+        #     elif diff == num and nums.count(diff) > 1:    
+        #         twoSumIndices = [i for i,n in enumerate(nums) if n == diff]
+        #         return twoSumIndices
             
