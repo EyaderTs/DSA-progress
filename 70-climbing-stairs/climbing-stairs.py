51e20a30eb88dc53053680451d52fc2cedff60ac
Eyader Tsehayu <eyadertsehayu@gmail.com>
2026-02-12 13:07:45 +0300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (87.74%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        dp=[float("inf")]*(n+1)
+        #base cases
+        dp[0] = 0
+        dp[1] = 1
+
+        if n>=2:
+            dp[2] = 2
+        
+        for i in range (3,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n] 
+        
