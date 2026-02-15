259509a910905f07f29ee51e754b716abc27d889
Eyader Tsehayu <eyadertsehayu@gmail.com>
2026-02-15 15:20:43 +0300

    Time: 75 ms (58.96%) | Memory: 21.3 MB (69.07%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        n=len(nums)
+        dp = [0]*len(nums)
+
+        for i,n in enumerate (nums):
+            dp[i] = max(n, n+dp[i-1])
+        # print(dp)
+        return max(dp)      
